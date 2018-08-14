b1776f46b0c66a4471593628355e80474b771907
Yuta Watanabe <ywatanab0301@gmail.com>
2018-08-14 17:02:22 +0900

    Created articles table
    Created articles table

--- db/migrate/20180814074529_create_articles.rb
+++ db/migrate/20180814074529_create_articles.rb
@@ -1,8 +1,12 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-
-      t.timestamps null: false
+      t.string           :name
+      t.string           :title
+      t.datetime         :date_time
+      t.string           :content
+      t.integer          :user_id
+      t.timestamps
     end
   end
 end
