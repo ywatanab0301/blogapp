d53035d6ece57078159873164cb00a6f25cd2db4
Yuta Watanabe <ywatanab0301@gmail.com>
2018-08-15 19:02:54 +0900

    Add edit action in articles controller
    Add edit action in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.create(title: articles_params[:title], content: articles_params[:content], user_id: current_user.id)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   private
   def articles_params
     params.require(:article).permit(:title, :content)
