4a10862c0237fcb2c19366e77f678743e698008b
Yuta Watanabe <ywatanab0301@gmail.com>
2018-08-16 23:39:38 +0900

    edit column in articles table
    edit column in articles table

--- db/migrate/20180814074529_create_articles.rb
+++ db/migrate/20180814074529_create_articles.rb
@@ -1,10 +1,8 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.string           :name
       t.string           :title
-      t.datetime         :date_time
-      t.string           :content
+      t.text             :content
       t.integer          :user_id
       t.timestamps
     end
