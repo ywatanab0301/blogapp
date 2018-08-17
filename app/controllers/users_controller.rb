da1abdfe280e35f036087f6c120501f7190fa0e9
Yuta Watanabe <ywatanab0301@gmail.com>
2018-08-17 13:22:26 +0900

    add link to each user page
    add link to each user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @username = user.username
-    @articles = current_user.articles.page(params[:page]).per(6)
+    @articles = user.articles.page(params[:page]).per(6)
   end
 end
