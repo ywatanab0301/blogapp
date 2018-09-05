20ba631651e9b1bcaeeaf429480b5c2c110461c6
Yuta Watanabe <ywatanab0301@gmail.com>
2018-09-05 15:51:05 +0900

    Define association for tag-function

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+  has_many :article_tags, dependent: :destroy
+  has_many :tags, through: :article_tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ActiveRecord::Base
+  has_many :article_tags, dependent: :destroy
+  has_many :articles, through: :article_tags
 end
