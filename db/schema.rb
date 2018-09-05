6249d85cc1b4264e612b7d573aa57ff9092d6483
Yuta Watanabe <ywatanab0301@gmail.com>
2018-09-05 15:39:09 +0900

    Create tables (tags table and article_tags table)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180816144539) do
+ActiveRecord::Schema.define(version: 20180905062644) do
+
+  create_table "article_tags", force: :cascade do |t|
+    t.integer  "article_id", limit: 4
+    t.integer  "tag_id",     limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "article_tags", ["article_id"], name: "index_article_tags_on_article_id", using: :btree
+  add_index "article_tags", ["tag_id"], name: "index_article_tags_on_tag_id", using: :btree
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -28,6 +38,14 @@ ActiveRecord::Schema.define(version: 20180816144539) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -48,4 +66,6 @@ ActiveRecord::Schema.define(version: 20180816144539) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "article_tags", "articles"
+  add_foreign_key "article_tags", "tags"
 end
